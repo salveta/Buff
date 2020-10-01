9fa961b1f0271f2fa924f2ae2c7372d29645a937
Salva <salvador.perez@gft.com>
2020-10-01 15:44:16 +0200

    "run app"

---
+++ SportBuff/buffsdk/src/main/java/com/buffup/sdk/ui/BuffView.kt
@@ -0,0 +1,9 @@
+package com.buffup.sdk.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class BuffView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
+
+}
